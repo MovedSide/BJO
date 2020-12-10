8a8cace113715ac396ddda58741649224a009bb5
moveside <jekker77@gmail.com>
2020-12-10 21:17:08 +0900

    1069 clear

---
+++ baekjoon_code/1069.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    double X, Y, D, T;
+    cin >> X >> Y >> D >> T;
+    double r,dis;
+    double cnt;
+    dis = sqrt(pow(X, 2) + pow(Y, 2));
+    r = dis;
+    cnt = r;
+    int jump=0;
+     while (r >= D)
+     {
+         r -= D;
+         jump++;
+     }
+     if (jump == 0)
+     {
+         cnt = min(T + 2, cnt);
+         cnt = min(T + D - r, cnt);
+     }
+    cnt = min(jump * T + r , cnt);
+    cnt = min(jump * T + T , cnt);    
+
+    cout << fixed;
+    cout.precision(13);
+    cout << cnt;
+    return 0;
+}

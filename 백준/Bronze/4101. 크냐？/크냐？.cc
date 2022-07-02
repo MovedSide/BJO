acde862b7c81bfd9a51f8af9ed0a2073d2ca71f8
Dongyeob Lee <35293542+moveside@users.noreply.github.com>
2022-07-02 18:43:43 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main()
+{
+	while (1)
+	{
+		int a, b;
+		cin >> a >> b;
+		if (a == 0 && b == 0) break;
+		if (a > b) cout << "Yes" << endl;
+		else cout << "No" << endl;
+	}
+	return 0;
+}
